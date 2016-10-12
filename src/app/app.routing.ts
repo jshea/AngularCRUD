c51b7cd997a8b10c028b0d347a0db68337480519
jshea <jshea@jpl.nasa.gov>
2016-10-12 07:51:54 -0700

    Improve routing (remove unused sub module route from top module router)

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,21 +1,15 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PersonComponent } from './person';
-
 const appRoutes: Routes = [
   {
     path: '',
-    redirectTo: '/person',
+    redirectTo: 'person',
     pathMatch: 'full'
   },
   {
     path: 'about',
     loadChildren: './about/about.module#AboutModule'
-  },
-  {
-    path: 'person',
-    component: PersonComponent
   }
 ];
 
